66bad3f6b34cffb2d09035423eecd66219a7ef91
toshiyuki <shimadat@sheets-design.com>
2024-12-01 13:44:58 +0900

    サブフォルダーにアップ

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+const subfolder = '/umeya/'
+export default defineConfig({
+    base: subfolder,
+    outDir: `./dist${subfolder}`,
+
+});
